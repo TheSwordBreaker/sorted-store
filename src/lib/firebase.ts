0186cd2215fceeb9b5ff67b1c57a00f292789fa3
theSwordBreaker <kunalguptadev007@gmail.com>
2023-02-20 01:35:06 +0530

    firebase name changed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp, getApp } from "Firebase/app";
+import { initializeApp, getApp } from "firebase/app";
 
 import { getAuth, updateProfile, updateEmail } from "firebase/auth";
 const FirebaseCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
 };
 // if a Firebase instance doesn't exist, create one
 
